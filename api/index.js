3907f1629900f623634f27daf699cec756afdd14
ACastilloSantiago <santiagorodrigec@gmail.com>
2023-11-07 17:42:56 -0300

    config back

--- api/index.js
+++ api/index.js
@@ -17,12 +17,14 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require("dotenv").config();
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const getTemperaments = require("./src/controllers/getTemperaments.js");
+const { PORT } = process.env;
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     getTemperaments();
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
